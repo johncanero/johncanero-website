80fc0559990a66ded0ec9038900247ef1f659f4d
John Canero <johncanero.official@gmail.com>
2022-11-08 21:25:33 +0800

    Add Font Poppins

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="mt-16">
+    <section className="mt-16 font-Poppins">
       <h1 className="text-7xl font-bold">
         Hi I'm <span className="dark:text-purple-600">Jam</span>
       </h1>
