91201fe661a695d7843bbeab4c700dd0897f99ad
John Canero <johncanero.official@gmail.com>
2022-11-08 20:34:08 +0800

    Apply Style Layer at globals.css and Header MOdify

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <header className='h-16 flex items-center justify-between bg-red-50'>
+    <header className='h-16 flex items-center justify-between'>
         <ul className='flex gap-4'>
             <Link href={'/'}>Home</Link>
             <Link href={'/about'}>About</Link>
