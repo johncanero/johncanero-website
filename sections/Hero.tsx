32f5b7f6fe909ecb432ea7a9cb50f702698b511c
John Canero <johncanero.official@gmail.com>
2022-11-09 12:26:09 +0800

    LinkedIn Button Hire Me Tag

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -7,15 +8,19 @@ const Hero = () => {
       <h1 className="sm:text-6xl flex sm:justify-center md:justify-start md:text-7xl font-bold">
         Hi I'm <span className="dark:text-purple-600 ml-2">Jam</span>
       </h1>
-      <h3 className="flex my-3 mb-4 md:mb-2 sm:text-2xl sm:justify-center md:justify-start md:text-4xl">I am Web Developer</h3>
+      <h3 className="flex my-3 mb-4 md:mb-2 sm:text-2xl sm:justify-center md:justify-start md:text-4xl">
+        I am Web Developer
+      </h3>
       <p className="flex mb-8 mx-4 md:mx-0 sm:text-center md:text-left text-gray-700 md:mr-48 lg:mr-96">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
         quibusdam autem doloremque beatae iure, nihil fugit doloribus cum soluta
         modi!
       </p>
-      <button
-        className=" flex sm:mx-auto md:mx-0 bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-lg"
-      >Hire Me!</button>
+      <Link href="https://www.linkedin.com/feed/" target="_blank"  rel="noopener noreferrer">
+        <button className=" flex sm:mx-auto md:mx-0 bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-lg">
+          Hire Me!
+        </button>
+      </Link>
     </section>
   );
 };
