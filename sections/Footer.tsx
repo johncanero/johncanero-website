50711387081f1784b0390b8fb48f2cf158cf4991
John Canero <johncanero.official@gmail.com>
2022-11-09 12:28:57 +0800

    Footer Tag Styling

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="py-6 text-center text-gray-500 text-sm">
+      <span className="dark:text-gray-100 text-gray-900 font-bold text-lg mr-2">
+        John Cañero
+      </span>
+      &copy; {new Date().getFullYear()} All Rights Reserved
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
