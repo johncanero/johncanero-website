3c9cdc842659baabec0fd8ccf5a0940d616e7565
John Canero <johncanero.official@gmail.com>
2022-11-09 23:35:39 +0800

    Change Color Button

--- components/MenuDropdown.tsx
+++ components/MenuDropdown.tsx
@@ -1,6 +1,6 @@
 import { Menu, Transition } from "@headlessui/react";
 import { Fragment, useEffect, useRef, useState } from "react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { Bars3Icon } from "@heroicons/react/20/solid";
 
 import React from "react";
 
@@ -8,12 +8,11 @@ const MenuDropdown = () => {
   return (
     // Mobile Visiblity
     <div className="block md:hidden top-16 w-96 mt-3 text-right">
-      <Menu as="div" className="relative inline-block text-left mr-6">
+      <Menu as="div" className="relative inline-block text-left mr-7">
         <div>
-          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black px-4 py-2 text-sm font-medium text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-white">
-            Dark Mode
-            <ChevronDownIcon
-              className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
+          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 px-2 py-2 text-sm font-medium text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-white">
+            <Bars3Icon
+              className="h-5 w-5 text-white"
               aria-hidden="true"
             />
           </Menu.Button>
