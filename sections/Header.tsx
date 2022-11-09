7f81caa83bf4221bf54c2be9e9118cd95189f564
John Canero <johncanero.official@gmail.com>
2022-11-09 23:29:32 +0800

    Change File JS to tsx

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Button from "../components/Button";
-import MenuDropdown from "../components/MenuDropdown";
+import MenuDropdown from "../components/MenuDropdown.tsx";
 import { useTheme } from "next-themes";
 import { useState, useEffect } from "react";
 
