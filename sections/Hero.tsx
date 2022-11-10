e4304abe1fb9bd2a2262b705e39a1e2bd1662f7a
John Canero <johncanero.official@gmail.com>
2022-11-10 16:25:22 +0800

    Change Font Size Desktop Welcome Phrase

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -6,15 +6,15 @@ import Link from "next/link";
 const Hero = () => {
   return (
     <div className="mt-16 font-Poppins lg:mr-60">
-      <h1 className="sm:text-6xl text-center md:justify-start md:text-left md:text-7xl font-bold">
+      <h1 className="sm:text-6xl text-center md:justify-start md:text-left md:text-7xl lg:text-8xl font-bold">
         Hi I'm
         <span className="dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-cyan-500 dark:to-blue-500 ml-2">
-          Jam
+          Jam 
         </span>
       </h1>
       
-      <h3 className="text-center my-3 mt-3 mb-5 text-xl mx-4 md:mx-0 md:mt-4 md:mb-3 md:text-left md:justify-start md:text-3xl lg:mr-48">
-        I am Frontend Engineer, Web Developer & Architectural Designer
+      <h3 className="text-center my-3 mt-3 mb-5 text-xl mx-4 md:mx-0 md:mt-4 md:mb-4 md:text-left md:justify-start md:text-3xl lg:mr-48">
+        I am a Frontend Engineer, Web Developer & Architectural Designer
       </h3>
 
       <p className="text-center mb-10 mx-6 md:mb-8 md:mx-0 sm:text-center md:text-left text-gray-600 md:mr-48 lg:mr-96">
