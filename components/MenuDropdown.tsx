52efc6f4d6daca843b8db15f8c9e751925d6c331
John Canero <johncanero.official@gmail.com>
2022-11-10 16:14:45 +0800

    Add Twitter Icon and Link

--- components/MenuDropdown.tsx
+++ components/MenuDropdown.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import Button from "./Button";
 import { Menu, Transition } from "@headlessui/react";
 import { Fragment, useEffect, useRef, useState } from "react";
@@ -8,6 +9,7 @@ import { useTheme } from "next-themes";
 import { Bars3Icon } from "@heroicons/react/20/solid";
 import { FaHome } from "@react-icons/all-files/fa/FaHome";
 import { IoMdPerson } from "@react-icons/all-files/io/IoMdPerson";
+import { FaTwitter } from "@react-icons/all-files/fa/FaTwitter";
 
 const MenuDropdown = () => {
   const { systemTheme, theme, setTheme } = useTheme();
@@ -63,57 +65,79 @@ const MenuDropdown = () => {
 
   return (
     // Mobile Visiblity
-    <div className="block md:hidden top-16 w-96 mt-3 text-right">
-      {renderThemeChanger()}
-      <Menu as="div" className="relative inline-block text-left mr-7">
-        <div>
-          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 px-2 py-2 text-sm font-medium text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-white">
-            <Bars3Icon className="h-5 w-5 text-white" aria-hidden="true" />
-          </Menu.Button>
-        </div>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-            {/* Home and About Section */}
-            <div className="px-1 py-1 ">
-              <Menu.Item>
-                {({ active }) => (
-                  <Link href="/">
-                    <button
-                      className={`${
-                        active ? "bg-violet-500 text-white" : "text-gray-900"
-                      } group flex w-full items-center rounded-md px-2 py-2 text-sm font-Poppins`}
+    <div className="flex justify-between md:hidden top-16 w-96 mt-3 text-right">
+      {/* Image */}
+      <div>
+        <Image src="/images/letterJ.png" width={33} height={33} alt="jLogo" />
+      </div>
+      {/* Dark Mode and Toggle */}
+      <div>
+        {renderThemeChanger()}
+        <Menu as="div" className="relative inline-block text-left mr-7">
+          <div>
+            <Menu.Button className="inline-flex w-full justify-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 px-2 py-2 text-sm font-medium text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-white">
+              <Bars3Icon className="h-5 w-5 text-white" aria-hidden="true" />
+            </Menu.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              {/* Home and About Section */}
+              <div className="px-1 py-1 ">
+                <Menu.Item>
+                  {({ active }) => (
+                    <Link href="/">
+                      <button
+                        className={`${
+                          active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm font-Poppins`}
+                      >
+                        <FaHome className="mr-1" size={17} /> Home
+                      </button>
+                    </Link>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <Link href="/about">
+                      <button
+                        className={`${
+                          active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex w-72 items-center rounded-md px-2 py-2 text-sm font-Poppins`}
+                      >
+                        <IoMdPerson className="mr-1" size={17} /> About
+                      </button>
+                    </Link>
+                  )}
+                </Menu.Item>
+              </div>
+              {/* Create a New Section */}
+              <div className="px-1 py-1 pb-2">
+                <Menu.Item>
+                  {({ active }) => (
+                    <Link
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://twitter.com/johncaneroo"
+                      className="flex ml-3 font-Poppins text-sm text-gray-900"
                     >
-                      <FaHome className="mr-1" size={17} /> Home
-                    </button>
-                  </Link>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <Link href="/about">
-                    <button
-                      className={`${
-                        active ? "bg-violet-500 text-white" : "text-gray-900"
-                      } group flex w-72 items-center rounded-md px-2 py-2 text-sm font-Poppins`}
-                    >
-                      <IoMdPerson className="mr-1" size={17} /> About
-                    </button>
-                  </Link>
-                )}
-              </Menu.Item>
-            </div>
-            {/* Create a New Section */}
-          </Menu.Items>
-        </Transition>
-      </Menu>
+                      <FaTwitter className="mr-1 text-cyan-400 " size={17} /> @johncanero
+                    </Link>
+                  )}
+                </Menu.Item>
+           
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      </div>
     </div>
   );
 };
