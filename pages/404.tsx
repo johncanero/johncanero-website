07037f064c195c8fa1cd9292ea2d423dc5ba9f22
John Canero <johncanero.official@gmail.com>
2022-11-15 11:35:11 +0800

    Page Not Found Logo Picture

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+
 const NotFound = () => {
-    return (
-      <div className="text-center font-Poppins text-gray-600 font-semibold mt-20 text-2xl">Oops! page not found 😢</div>
-    )
-  }
-  
-  export default NotFound
+  return (
+    <div>
+      <div className="flex justify-center mt-6 ">
+        <Image
+          src="/images/404Icon.png"
+          width={800}
+          height={800}
+          alt="Udemy Certificate"
+          className="rounded-2xl transition ease-in-out  hover:-translate-y-1 hover:scale-105 duration-300"
+        />
+      </div>
+
+      <div className="text-center font-Poppins text-gray-600 font-semibold text-xl">
+        Oops! page not found 😢
+      </div>
+
+    </div>
+  );
+};
+
+export default NotFound;
