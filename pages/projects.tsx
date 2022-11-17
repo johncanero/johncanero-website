9ba499895f244e35f5e0f117fd33ad6626412fe7
John Canero <johncanero.official@gmail.com>
2022-11-18 00:17:26 +0800

    Airbnb Clone Card

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -11,9 +11,9 @@ const projects = () => {
         Projects
       </div>
 
-      <div className="mt-6">
+      <div className="md:flex md:gap-12 mt-8">
         {/* Remote Landing Page Card */}
-        <div className="max-w-sm rounded-2xl overflow-hidden shadow-2xl dark:shadow-lg dark:shadow-slate-50 bg-zinc-100 dark:bg-zinc-200 font-Poppins">
+        <div className="max-w-xs mx-auto md:mx-0 rounded-2xl overflow-hidden shadow-2xl dark:shadow-md dark:shadow-slate-50 bg-zinc-100 dark:bg-zinc-200 font-Poppins">
           <div className="px-4 py-4">
             <Link
               target="_blank"
@@ -46,6 +46,44 @@ const projects = () => {
             </span>
             <span className="inline-block bg-gray-200 dark:bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 dark:text-gray-800 mr-2 mb-2">
               #work
+            </span>
+          </div>
+        </div>
+
+        {/* Airbnb Clone */}
+        <div className="max-w-xs mx-auto md:mx-0  rounded-2xl overflow-hidden shadow-2xl dark:shadow-md dark:shadow-slate-50 bg-zinc-100 dark:bg-zinc-200 font-Poppins mt-9 md:mt-0">
+          <div className="px-4 py-4">
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://airbnb-clone-by-johncanero.vercel.app/"
+            >
+              <Image
+                src="/projects/airBnbClone2.png"
+                width={250}
+                height={250}
+                alt="Airbnb Clone"
+                className="w-full animate__animated animate__fadeIn"
+              />
+            </Link>
+            <div className="font-bold text-xl mt-2 mb-2 dark:text-black">
+              Airbnb Clone
+            </div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, nulla! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div className="px-4 mt-2 pb-2">
+            <span className="inline-block bg-gray-200 dark:bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 dark:text-gray-800 mr-2 mb-2">
+              #airbnb
+            </span>
+            <span className="inline-block bg-gray-200 dark:bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 dark:text-gray-800 mr-2 mb-2">
+              #clone
+            </span>
+            <span className="inline-block bg-gray-200 dark:bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 dark:text-gray-800 mr-2 mb-2">
+              #feature
             </span>
           </div>
         </div>
