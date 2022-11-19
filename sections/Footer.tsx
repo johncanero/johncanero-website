49a2e26a387f795bebbda8d852df14f9520c348d
John Canero <johncanero.official@gmail.com>
2022-11-20 00:25:28 +0800

    Adjust Footer All Rights Reserved for Mobile

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className=" py-14 md:py-6 text-center text-gray-500 text-sm">
+    <footer className="md:py-6 text-center text-gray-500 text-sm">
       <span className="dark:text-gray-100 text-gray-900 font-bold text-lg mr-2">
         John Cañero
       </span>
