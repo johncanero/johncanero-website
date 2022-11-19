ed8a98ac861aaebfdadd2b0c5b8dbbb29383488e
John Canero <johncanero.official@gmail.com>
2022-11-20 00:35:31 +0800

    Hover Icons

--- sections/MobileFooterButtons.tsx
+++ sections/MobileFooterButtons.tsx
@@ -13,35 +13,35 @@ const MobileFooterButtons = () => {
       <div className="flex justify-center fixed bottom-0 left-0 w-screen bg-gray-100 dark:bg-black  text-center gap-11 border border-t-1 border-slate-400 dark:border-slate-700 border-x-0 py-3 md:hidden ">
         {/* Home */}
         <Link href="/" rel="noopener noreferrer">
-          <button>
+          <button className=" hover:text-gray-800 dark:hover:text-gray-50">
             <FaHome size={25} />
           </button>
         </Link>
 
         {/* About Me */}
         <Link href="/about/about" rel="noopener noreferrer">
-          <button>
+          <button  className="hover:text-gray-800 dark:hover:text-gray-50">
             <IoMdPerson size={25} />
           </button>
         </Link>
 
         {/* Tech Stack */}
         <Link href="/techstack/techstack" rel="noopener noreferrer">
-          <button>
+          <button className="hover:text-gray-800 dark:hover:text-gray-50">
             <FaConnectdevelop size={25} />
           </button>
         </Link>
 
         {/* Projects */}
         <Link href="/projects" rel="noopener noreferrer">
-          <button>
+          <button className="hover:text-gray-800 dark:hover:text-gray-50">
             <AiOutlineProject size={25} />
           </button>
         </Link>
 
         {/* Bio Link*/}
         <Link href="https://bio.link/johncanero" rel="noopener noreferrer">
-          <button>
+          <button className="hover:text-gray-800 dark:hover:text-gray-50">
             <VscLinkExternal size={25} />
           </button>
         </Link>
