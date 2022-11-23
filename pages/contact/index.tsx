e5e58faf570f868381a9f534202acc7fe6ddb524
John Canero <johncanero.official@gmail.com>
2022-11-23 18:41:41 +0800

    Add Contact Text

---
+++ pages/contact/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const contact = () => {
+  return (
+    <div>
+      <div className="text-center text-gray-600 font-Poppins font-semibold mt-4 text-2xl">
+        Contact
+      </div>
+    </div>
+  );
+};
+
+export default contact;
