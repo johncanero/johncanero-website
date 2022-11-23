a693d6be7da216fd99a10f93c90682e64357bce4
John Canero <johncanero.official@gmail.com>
2022-11-24 00:55:31 +0800

    Add Exclamation = Hi There

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -14,7 +14,7 @@ const contact = () => {
             <form className="flex flex-col items-center w-1/3 mx-auto">
               <Input id="name" name="name" placeholder="Your Name" label="Name" />
               <Input id="email" name="name" placeholder="email@gmail.com" label="Email" />
-              <TextArea id="message" name="message" placeholder="Hi There" label="Your message" />
+              <TextArea id="message" name="message" placeholder="Hi There!" label="Your message" />
               <button className="py-2 mt-6 text-lg text-white bg-purple-500 rounded-md outline-none w-80 md:w-96 active:bg-purple-600 focus:ring-2 focus:ring-purple-400 disabled:bg-opacity-50 disabled:cursor-not-allowed" type="submit" 
               
               >Submit</button>
