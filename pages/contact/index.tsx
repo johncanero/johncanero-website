0f878606c390350923b9c81076bb85be38746d10
John Canero <johncanero.official@gmail.com>
2022-11-25 09:00:06 +0800

    Add Image Header Contact Form and Delete Unnecessary Images

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import Image from "next/image";
 
 const contact = () => {
- 
   return (
     <div>
       <div className="mt-4 text-2xl font-semibold text-center text-gray-600 font-Poppins">
         Contact
+      </div>
+
+      <div className="container flex justify-center px-6 mx-auto my-6">
+        <section className="mb-32 text-gray-800">
+          <div className="relative overflow-hidden bg-no-repeat bg-cover">
+            <Image
+              src="/images/contactBackground.jpg"
+              width={800}
+              height={800}
+              alt="contactBackground"
+              className="rounded-xl md:h-80"
+            />
+          </div>
+        </section>
       </div>
     </div>
   );
