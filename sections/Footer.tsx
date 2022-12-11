b6ad2573d0adb120b472eb4ed2fcfe2f97b6b5e7
John Canero <johncanero.official@gmail.com>
2022-12-11 10:25:21 +0800

    Update Footer Comments

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
+// Input Footer Name and All Rights Reserved text
+// Create a function for updated year 
+
 const Footer = () => {
   return (
     // Hide in Mobile
-    <footer className="hidden md:block md:py-6 text-center text-gray-500 text-sm">
-      <span className="dark:text-gray-100 text-gray-900 font-bold text-lg mr-2">
+    <footer className="hidden text-sm text-center text-gray-500 md:block md:py-6">
+      <span className="mr-2 text-lg font-bold text-gray-900 dark:text-gray-100">
         John Cañero
       </span>
       &copy; {new Date().getFullYear()} All Rights Reserved
