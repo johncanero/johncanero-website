cbe270f648d759fa0f3c0fd7f0caf8fbc989b9db
John Cañero <99738754+johncanero@users.noreply.github.com>
2022-12-13 19:57:31 +0800

    Add LinkedIn Profile at Footer: John Cañero

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 // Input Footer Name and All Rights Reserved text
 // Create a function for updated year 
@@ -7,9 +8,15 @@ const Footer = () => {
   return (
     // Hide in Mobile
     <footer className="hidden text-sm text-center text-gray-500 md:block md:py-6">
+       <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.linkedin.com/in/johncanero/"
+      >
       <span className="mr-2 text-lg font-bold text-gray-900 dark:text-gray-100">
         John Cañero
       </span>
+      </Link>
       &copy; {new Date().getFullYear()} All Rights Reserved
     </footer>
   );
