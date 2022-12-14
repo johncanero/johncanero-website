547329bf7129a59253527b689abda8e8ad70da65
John Canero <johncanero.official@gmail.com>
2022-12-14 11:39:47 +0800

    Adjust Width and Header

--- pages/visuals/index.tsx
+++ pages/visuals/index.tsx
@@ -10,7 +10,7 @@ const index = () => {
   return (
     <div className="font-Poppins">
       {/* Add Links - Architecture */}
-      <div className="flex justify-end gap-3 mt-4 font-semibold">
+      <div className="flex justify-end gap-3 mt-4 text-sm font-semibold md:text-base">
         <Link rel="noopener noreferrer" href="/projects">
           <div className="flex gap-1 border-b-2 border-transparent dark:text-gray-300 hover:border-gray-600 dark:hover:border-gray-300">
             <AiOutlineLaptop className="mt-1" size={18} /> Tech Projects
@@ -36,16 +36,16 @@ const index = () => {
       </div>
 
       {/* Visuals */}
-      <div className="justify-center mt-6 mb-10 md:flex md:flex-wrap md:gap-12">
+      <div className="flex justify-center mt-6 mb-10 md:flex md:flex-wrap md:gap-12">
         <div>
           <Image
             src="/images/visuals/escapeCompetition.jpg"
             width={300}
             height={40}
             alt="escapeCompetition"
-            className="animate__animated animate__fadeIn"
+            className="animate__animated animate__fadeIn md:w-60"
           />
-          <p className="">Escape Competition through Authenticity 🧬</p>
+          <p className="text-sm font-bold text-center w-60">Escape Competition through Authenticity</p>
         </div>
       </div>
     </div>
