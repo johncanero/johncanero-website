b9e102634e5de8fa8c2f9976cf06d9866437d87f
John Canero <johncanero.official@gmail.com>
2022-12-17 11:37:59 +0800

    Change Color of Mobile Droption

--- components/MenuDropdown.tsx
+++ components/MenuDropdown.tsx
@@ -86,7 +86,7 @@ const MenuDropdown = () => {
         {renderThemeChanger()}
         <Menu as="div" className="relative inline-block text-left mr-7">
           <div>
-            <Menu.Button className="inline-flex justify-center w-full px-2 py-2 text-sm font-medium text-white rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-white">
+            <Menu.Button className="inline-flex justify-center w-full px-2 py-2 text-sm font-medium text-white rounded-md bg-gradient-to-r from-neutral-800 to-neutral-900 dark:from-cyan-500 dark:to-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-white">
               <Bars3Icon className="w-5 h-5 text-white" aria-hidden="true" />
             </Menu.Button>
           </div>
