0007425f2964a2f31238d0ee4f3e25ed2fb4f107
John Canero <johncanero.official@gmail.com>
2022-12-21 08:10:04 +0800

    Add Compressed Image2

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -23,7 +23,7 @@ const contact = () => {
           {/* Background Image */}
           <div className="relative overflow-hidden bg-no-repeat bg-cover rounded-t-2xl">
             <Image
-              src="/images/contactBackgroundCompressed.jpg"
+              src="/images/contactBackgroundCompressed2.jpg"
               width={1150}
               height={800}
               alt="contactBackground"
