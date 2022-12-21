d7bd902b797297da9f10d9707ebee7afc188dd1b
John Canero <johncanero.official@gmail.com>
2022-12-21 08:18:58 +0800

    Hover Icons

--- sections/MobileFooterButtons.tsx
+++ sections/MobileFooterButtons.tsx
@@ -13,7 +13,7 @@ const MobileFooterButtons = () => {
       <div className="fixed bottom-0 left-0 flex justify-center w-screen py-2 ml-1 text-center bg-gray-100 border gap-7 sm:w-96 dark:bg-black50 border-t-1 border-slate-400 dark:border-slate-700 border-x-0 md:hidden ">
         {/* Home */}
         <Link href="/" rel="noopener noreferrer">
-          <button className="hover:text-gray-800 dark:hover:text-gray-50">
+          <button className="hover:text-neutral-700 dark:hover:text-gray-50">
             <FaHome className="ml-1" size={23} />
             <p className="mt-1 text-xs text-center font-Poppins">Home</p>
           </button>
@@ -21,7 +21,7 @@ const MobileFooterButtons = () => {
 
         {/* About Me */}
         <Link href="/about" rel="noopener noreferrer">
-          <button className="hover:text-gray-800 dark:hover:text-gray-50">
+          <button className="hover:text-neutral-700 dark:hover:text-gray-50">
             <IoMdPerson className="ml-2" size={23} />
             <p className="mt-1 text-xs text-center font-Poppins">About</p>
           </button>
@@ -29,7 +29,7 @@ const MobileFooterButtons = () => {
 
         {/* Tech Stack */}
         <Link href="/techstack" rel="noopener noreferrer">
-          <button className="hover:text-gray-800 dark:hover:text-gray-50">
+          <button className="hover:text-neutral-700 dark:hover:text-gray-50">
             <FaConnectdevelop className="ml-5" size={23} />
             <p className="mt-1 text-xs text-center font-Poppins">Tech Stack</p>
           </button>
@@ -37,7 +37,7 @@ const MobileFooterButtons = () => {
 
         {/* Contact */}
         <Link href="/contact" rel="noopener noreferrer">
-          <button className="hover:text-gray-800 dark:hover:text-gray-50">
+          <button className="hover:text-neutral-700 dark:hover:text-gray-50">
             <AiOutlineMail className="ml-3" size={23} />
             <p className="mt-1 text-xs text-center font-Poppins">Contact</p>
           </button>
@@ -45,7 +45,7 @@ const MobileFooterButtons = () => {
         
         {/* Projects */}
         <Link href="/projects" rel="noopener noreferrer">
-          <button className="hover:text-gray-800 dark:hover:text-gray-50">
+          <button className="hover:text-neutral-700 dark:hover:text-gray-50">
             <AiOutlineProject className="ml-2" size={23} />
             <p className="mt-1 text-xs text-center font-Poppins">Projects</p>
           </button>
