df11e803ae503effeefe072723d34b44515382a1
John Canero <johncanero.official@gmail.com>
2022-12-22 11:06:41 +0800

    Change Margin Padding

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -33,7 +33,7 @@ const Hero = () => {
         </div>
       </h3>
 
-      <p className="mx-10 mb-10 text-center text-gray-600 md:mb-8 md:mx-0 sm:text-center md:text-left dark:text-gray-500 md:mr-48">
+      <p className="mb-10 text-center text-gray-600 mx-11 md:mb-8 md:mx-0 sm:text-center md:text-left dark:text-gray-500 md:mr-48">
         I enjoy creating stuffs focusing on product design and development. <span className="font-bold">
           I like to BUILD. </span> To have fun, help people and show my work to the world.
         🏂
