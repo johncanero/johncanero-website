5af9eb356e69d40d46975eebf1ef017d82bae127
John Canero <johncanero.official@gmail.com>
2022-12-31 09:49:47 +0800

    Create outDir for generated files of siteMap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://johncanero-website.vercel.app/',
-    generateRobotsTxt: true, 
+    generateRobotsTxt: true,  
+    outDir: "./out"
   }
