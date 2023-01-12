b2e1f64e0dc1760962120ffe405ffde9ed7b35ec
John Canero <johncanero.official@gmail.com>
2023-01-12 11:37:06 +0800

    Improve SEO Homepage

--- components/Layout.js
+++ components/Layout.js
@@ -14,16 +14,20 @@ const Layout = ({ children }) => {
           name="description"
           content="John Cañero, John Canero, johncanero, Jam, Website, Portfolio"
         />
-        <meta name="google-site-verification" content="Am960gqQWDsLPwSXrZnAOe2asTMS73ZO7vHRgZ1lJwo" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="John Cañero, Home, Introduction, Portfolio and Social Media"/>
+        <meta name="description" content="John Cañero, Home, Introduction, Portfolio and Social Media, I am a Software Engineer, Web Developer &
+          Architectural Designer" />
         <meta property="og:title" content="John Cañero" />
         <meta property="og:description" content="John Cañero Website and Portfolio" />
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta property="og:url" content="https://johncanero.com/" />
+        <meta property="og:type" content="website"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="shortcut icon" href="/images/johnCaneroLogo5.png" />
+
+        {/* Google Verification and XML Sitemap */}
+        <meta name="google-site-verification" content="Am960gqQWDsLPwSXrZnAOe2asTMS73ZO7vHRgZ1lJwo" />
         <link rel="sitemap" type="application/xml" href="/sitemap-0.xml" />
       </Head>
-
       {/* Header, Main & Footer */}
       <div className="flex flex-col min-h-screen">
         <Header />
@@ -31,7 +35,6 @@ const Layout = ({ children }) => {
         <Footer />
         <MobileFooterButtons />
       </div>
-     
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import Head from "next/head";
 import Hero from "../sections/Hero";
 
 
 export default function Home() {
   return (
     <div>
-      {/* Main Content */}
+      {/* Head */}
+      <Head>
+        <title>John Cañero</title>
+        <meta
+          name="description"
+          content="John Cañero, John Canero, johncanero, Jam, Website, Portfolio"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="John Cañero, Home, Introduction, Portfolio and Social Media, I am a Software Engineer, Web Developer &
+          Architectural Designer" />
+        <meta property="og:title" content="John Cañero" />
+        <meta property="og:description" content="John Cañero Website and Portfolio" />
+        <meta property="og:url" content="https://johncanero.com/" />
+        <meta property="og:type" content="website"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="Am960gqQWDsLPwSXrZnAOe2asTMS73ZO7vHRgZ1lJwo" />
+        <link rel="shortcut icon" href="/images/johnCaneroLogo5.png" />
+        <link rel="sitemap" type="application/xml" href="/sitemap-0.xml" />
+      </Head>
       <Hero />
     </div>
   )
